e9008e4fb919e996569f004e627f1d8517e28b6e
Doniben <doniben@esperanto.co>
2023-04-06 15:56:37 -0600

    basic start excercize with styles

--- 04-react-event/goit-event/src/App.jsx
+++ 04-react-event/goit-event/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import goitLogo from './assets/Goit-logo.png'
 import './App.css'
-import ListRenderer from './components/ListRenderer'
-import { FunctionalListRenderer } from './components/FunctionalListRenderer'
 
 function App() {
 
@@ -16,10 +15,11 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://goit.global/ph/" title="GoIT Global" aria-label="GoIT Global">
+          <img src={goitLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Vite + React + GOIT</h1>
-      <ListRenderer />
-      {/* <FunctionalListRenderer /> */}
     </div>
   )
 }
